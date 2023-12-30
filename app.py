b24c7097a4dcdfa7dd02737d4d8e2de342c0a556
Baian Liu <bliu989@gmail.com>
2023-12-30 12:44:30 -0500

    updated Streamlit app to use cache

--- app.py
+++ app.py
@@ -7,26 +7,32 @@ import os
 from PIL import Image
 from matplotlib import image
 
-model = load_model('bird_classifier.h5')
+@st.cache_resource
+def get_model():
+    return load_model('bird_classifier.h5')
 
-# load images as pixel array
-images = []
+@st.cache_data
+def get_images():
+    images = []
+    for i in range(1, 9):
+        filename = f'{i}.jpg'
+        img = image.imread(os.path.join('res', filename))
+        images.append(img)
+    return images
 
-for i in range(1, 9):
-    filename = f'{i}.jpg'
-    img = image.imread(os.path.join('res', filename))
-    images.append(img)
- 
-with open('labels.txt', 'r') as fp:
-    labels = fp.read().split('\n')
+@st.cache_data
+def get_labels():
+    with open('labels.txt', 'r') as fp:
+        labels = fp.read().split('\n')
+    return labels
 
 def top_5_pred(input):
     preds = model.predict(tf.convert_to_tensor([input]))[0]
     pred_indices = np.argpartition(preds, -5)[-5:]
     pred_species = pred_indices[np.argsort(preds[pred_indices])][::-1]
-    pred_text = '\n'.join([f'{preds[i]:.2%} {labels[i]}' for i in pred_species])
-    
+    pred_text = '\n'.join([f'{preds[i]:.2%} {labs[i]}' for i in pred_species])
     return pred_text
+
 
 st.set_page_config(layout='wide')
 st.title('Bird species classifier')
@@ -43,25 +49,16 @@ st.write("""This model classifies images of birds into one of 525 species.
                 For the best results, use a photo that only contains one bird 
                 that takes up most of the photo. This model has a 89.73% test 
                 accuracy and a 97.45% top 5 test accuracy.""")
-# with st.sidebar:
-#     st.write("""This model classifies images of birds into one of 525 species.
-#                 The dataset used to train the model can be found
-#                 [here](https://www.kaggle.com/datasets/gpiosenka/100-bird-species).
-#                 The model is a convolutional neural network (CNN) without any
-#                 transfer learning.
-#                 You can either select one of the bird pictures, which were
-#                 not used in training or validating the model, or
-#                 upload a bird picture to
-#                 get the 5 most likely species for that bird according to the model.
-#                 For the best results, use a photo that only contains one bird
-#                 that takes up most of the photo. \n \n This model has a 89.73% test
-#                 accuracy and a 97.45% top 5 test accuracy.""")
 
 col1, col2 = st.columns(2)
 
+model = get_model()
+imgs = get_images()
+labs = get_labels()
+
 with col1:
     img = image_select(label='Select a bird',
-                   images=images,
+                   images=imgs,
                    use_container_width=False)
     st.text_area('Predictions:', top_5_pred(img), height=150)
 

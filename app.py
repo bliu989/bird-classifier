1514a066015c2858a4abbab17acd9fb42c0e9a0c
Baian Liu <bliu989@gmail.com>
2024-02-22 10:24:03 -0500

    Reformatted streamlit app

--- app.py
+++ app.py
@@ -37,7 +37,15 @@ def top_5_pred(input):
 st.set_page_config(layout='wide')
 st.title('Bird species classifier')
 
-st.write("""This model classifies images of birds into one of 525 species.
+with st.sidebar:
+    st.write("""Birds encompass a diverse range of species. While some species
+                physically appear very different, there are many species that 
+                look very similar. Knowing which species of bird you've just
+                encountered can be a great way to get to know the nature around
+                you. 
+                """)
+
+    st.write("""This model classifies images of birds into one of 525 species.
                 The dataset used to train the model can be found 
                 [here](https://www.kaggle.com/datasets/gpiosenka/100-bird-species). 
                 The model is a convolutional neural network (CNN) built on top of
@@ -48,33 +56,46 @@ st.write("""This model classifies images of birds into one of 525 species.
                 get the 5 most likely species for that bird according to the model. 
                 For the best results, use a photo that only contains one bird 
                 that takes up most of the photo. This model has a 89.73% test 
-                accuracy and a 97.45% top 5 test accuracy.""")
+                accuracy and a 97.45% top 5 test accuracy. The code used to train
+                the model can be found
+                [here](https://github.com/bliu989/bird-classifier/blob/main/bird_classifier.ipynb).
+                """)
 
-col1, col2 = st.columns(2)
+style = '''
+<style>
+.block-container {
+    padding-top: 1rem;
+    padding-bottom: 0rem;
+}
+button[title="View fullscreen"] {
+    visibility: hidden;
+}
+</style>
+'''
+st.markdown(style, unsafe_allow_html=True)
 
 model = get_model()
 imgs = get_images()
 labs = get_labels()
 
-with col1:
-    img = image_select(label='Select a bird',
-                   images=imgs,
-                   use_container_width=False)
-    st.text_area('Predictions:', top_5_pred(img), height=150)
+cols = st.columns(8)
+col = 0
+for i in range(len(imgs)):
+    with cols[i]:
+        st.image(imgs[i], width=125)
 
-with col2:
-    uploaded_file = st.file_uploader('Upload a bird picture', type=['jpg', 'png'])
-    if uploaded_file is not None:
-        try:
-            im = Image.open(uploaded_file)
-            st.image(im)
-            im = np.array(im)
-            im = tf.image.resize(im, size=(224, 224))
+uploaded_file = st.file_uploader('Upload a bird picture or drag and drop an image from above', type=['jpg', 'png'])
+if uploaded_file is not None:
+    try:
+        im = Image.open(uploaded_file)
+        st.image(im)
+        im = np.array(im)
+        im = tf.image.resize(im, size=(224, 224))
 
-            st.text_area('Predictions:', top_5_pred(im), height=150)
+        st.text_area('Predictions:', top_5_pred(im), height=150)
 
-        except:
-            st.text_area('Predictions:',
-                         'Unable to perform predictions on this image',
-                         height=150)
+    except:
+        st.text_area('Predictions:',
+                     'Unable to perform predictions on this image',
+                     height=150)
 
